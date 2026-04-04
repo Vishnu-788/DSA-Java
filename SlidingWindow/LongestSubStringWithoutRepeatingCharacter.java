a45fdcc113c168877ad45c12f3025ad25c29d44d
Vishnu Prasad J S <prasadvishnu788@gmail.com>
2026-04-04 12:10:51 +0530

    Completed Longest Repeating Character Replacement. Leetcode: 424; Difficulty: Medium;

---
+++ SlidingWindow/LongestSubStringWithoutRepeatingCharacter.java
@@ -0,0 +1,11 @@
+package SlidingWindow;
+
+/*
+Longest Substring without repeating character Leetcode 3, Difficulty: Medium.
+*/
+
+public class LongestSubStringWithoutRepeatingCharacter {
+   public static void main(String[] args) {
+
+   }
+}
