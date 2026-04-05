158f96e4df1f24213b9128b45f93cf1f26cf0de8
Vishnu Prasad J S <prasadvishnu788@gmail.com>
2026-04-05 13:41:24 +0530

    Leetvode 3 complete;

--- SlidingWindow/LongestSubStringWithoutRepeatingCharacter.java
+++ SlidingWindow/LongestSubStringWithoutRepeatingCharacter.java
@@ -1,11 +1,37 @@
 package SlidingWindow;
 
+import java.util.HashSet;
+
 /*
 Longest Substring without repeating character Leetcode 3, Difficulty: Medium.
 */
 
 public class LongestSubStringWithoutRepeatingCharacter {
-   public static void main(String[] args) {
 
-   }
+	public int solution(String s) {
+		HashSet<Character> visited = new HashSet<>();
+		int ans = 0;
+		int left = 0;
+		int right = 0;
+
+		while (right < s.length()) {
+			char c = s.charAt(right);
+			while(visited.contains(c)){
+				visited.remove(s.charAt(left));
+				left++;
+			}
+			visited.add(c);
+			ans = Math.max((right-left)+1, ans);
+			right++;
+		}
+		return ans;
+	}
+
+	public static void main(String[] args) {
+		String s = "abatman";
+		LongestSubStringWithoutRepeatingCharacter obj =
+			new LongestSubStringWithoutRepeatingCharacter();
+		int ans = obj.solution(s);
+		System.out.println(ans);
+	}
 }
